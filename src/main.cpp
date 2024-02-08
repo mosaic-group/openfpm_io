0073f51f6432c896e7ffd01668b39e5fb82a1ca7
Serhii Yaskovets <yaskovet@mpi-cbg.de>
2024-02-08 22:29:01 +0100

    Remove install scripts from openfpm_io; move all scripts to pdata

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,6 @@ int main(int argc, char* argv[])
 #include "CSVWriter/CSVWriter_unit_tests.hpp"
 #include "GraphMLWriter/GraphMLWriter_unit_tests.hpp"
 #include "VTKWriter/VTKWriter_unit_tests.hpp"
-//#include "HDF5_XdmfWriter/HDF5_XdmfWriter_unit_tests.hpp"
 #include "Plot/Plot_unit_tests.hpp"
 #include "RawReader/RawReader_unit_tests.hpp"
 #include "HDF5_wr/HDF5_writer_unit_tests.hpp"
